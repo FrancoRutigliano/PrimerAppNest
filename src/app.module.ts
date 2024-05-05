bb548962314f6e55ea41bb124bd2efcc3e3c4cd3
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-04 23:17:08 -0300

    Configuración de carga de variables de entorno para la conexión a la base de datos
    Añade la carga de variables de entorno desde un archivo .env para mejorar la seguridad y la portabilidad de la configuración de la conexión a la base de datos en NestJS. Se ha agregado el paquete dotenv para gestionar las variables de entorno de forma más segura.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CatsModule],
+  imports: [CatsModule , ConfigModule.forRoot(), // carga variables de entorno
+    TypeOrmModule.forRoot({
+    type: "mysql",
+    host:"localhost",
+    port:3307,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE,
+    autoLoadEntities: true,
+    synchronize: true,
+  })],
   controllers: [],
   providers: [],
 })
