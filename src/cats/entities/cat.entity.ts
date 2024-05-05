b642c118add5d15715e92c0eeb8780f8f1079d02
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-04 23:40:09 -0300

    feat: desarrollo de la entidad cats

--- src/cats/entities/cat.entity.ts
+++ src/cats/entities/cat.entity.ts
@@ -1,1 +1,20 @@
-export class Cat {}
+import { Column, DeleteDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity() // decorador que define entidad
+export class Cat {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()    
+    name: string;
+
+    @Column()
+    age: string;
+
+    @Column()
+    breed: string;
+
+    // almacena fecha y hora de la eliminacion --> cuando se llama a metodo delete. No se elimina de la db
+    @DeleteDateColumn()
+    deleteDateColumn: Date;
+}
