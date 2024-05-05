f64cfb637b873372b617981e8591bdd76e08044f
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-04 21:03:05 -0300

    mappeando rutas de entidad cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CatsModule],
+  controllers: [],
+  providers: [],
 })
+
 export class AppModule {}
