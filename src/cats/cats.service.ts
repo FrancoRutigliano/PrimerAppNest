a119d31d35f80ba32e89a71af85246eab31b2847
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-06 10:35:13 -0300

    logica crud cats

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Cat } from './entities/cat.entity';
 import { Repository } from 'typeorm';
+import { Breed } from 'src/breeds/entities/breed.entity';
 
 @Injectable()
 export class CatsService {
@@ -12,25 +13,43 @@ export class CatsService {
     @InjectRepository(Cat)
     // en esta var se almacenará el Repo inyectado arriba
     private catRepository: Repository<Cat>,
+
+    @InjectRepository(Breed)
+    private breedsRepository: Repository<Breed>,
   ) {}
 
   async create(createCatDto: CreateCatDto) {
-    return 'this action adds a new cat';
+    const breed = await this.breedsRepository.findOneBy({
+      name: createCatDto.breed,
+    });
+
+    // si este name esta vacio devolvemos un error
+    if (!breed) {
+      throw new BadRequestException('Breed not found');
+    }
+
+    // crear en entidad cat el nuevo registro
+    const cat = this.catRepository.create([{
+      name: createCatDto.name,
+      age: createCatDto.age.toString(),
+    }]);    
+    //guardar el regitro dentro de la base de datos
+    return await this.catRepository.save(cat);
   }
 
-  findAll() {
-    return this.catRepository.find();
+  async findAll() {
+    return await this.catRepository.find();
   }
 
-  findOne(id: number) {
-    return this.catRepository.findOneBy({ id });
+  async findOne(id: number) {
+    return await this.catRepository.findOneBy({ id });
   }
 
-  update(id: number, updateCatDto: UpdateCatDto) {
-    return `this action updates a #${id} cat`;
+  async update(id: number, updateCatDto: UpdateCatDto) {
+    return await this.catRepository.update(id, updateCatDto);
   }
 
-  remove(id: number) {
-    return this.catRepository.delete(id);
+  async remove(id: number) {
+    return await this.catRepository.softDelete(id);
   }
 }
