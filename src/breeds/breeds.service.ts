226839b4907ff72c2e953037272eef77dacfe686
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-06 09:55:06 -0300

    creando el dto de breed y añadiendo lógica a breed.service

--- src/breeds/breeds.service.ts
+++ src/breeds/breeds.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBreedDto } from './dto/create-breed.dto';
 import { UpdateBreedDto } from './dto/update-breed.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Breed } from './entities/breed.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BreedsService {
-  create(createBreedDto: CreateBreedDto) {
-    return 'This action adds a new breed';
+  constructor(
+    @InjectRepository(Breed)
+    // almacenar el repo inyectado unicamente de lectura
+    private readonly breedRepository: Repository<Breed>,
+  ) {}
+  
+  
+  async create(createBreedDto: CreateBreedDto) {
+    const breed = this.breedRepository.create(createBreedDto);
+    return await this.breedRepository.save(breed);
   }
 
-  findAll() {
-    return `This action returns all breeds`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} breed`;
-  }
-
-  update(id: number, updateBreedDto: UpdateBreedDto) {
-    return `This action updates a #${id} breed`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} breed`;
+  async findAll() {
+    return await this.breedRepository.find();
   }
 }
