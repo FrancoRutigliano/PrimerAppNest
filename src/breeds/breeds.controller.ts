188f2c9d297aba16354b2ce7f16e66ed71ec9994
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-06 11:21:03 -0300

    corrigiendo el controller breeds

--- src/breeds/breeds.controller.ts
+++ src/breeds/breeds.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { BreedsService } from './breeds.service';
 import { CreateBreedDto } from './dto/create-breed.dto';
-import { UpdateBreedDto } from './dto/update-breed.dto';
 
 @Controller('breeds')
 export class BreedsController {
@@ -15,20 +14,5 @@ export class BreedsController {
   @Get()
   findAll() {
     return this.breedsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.breedsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBreedDto: UpdateBreedDto) {
-    return this.breedsService.update(+id, updateBreedDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.breedsService.remove(+id);
   }
 }
