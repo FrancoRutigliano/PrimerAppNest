ba5e45055521bbe7b4d7f9a6a4234af848824bc4
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-06 10:13:08 -0300

    comunicación entre modulos

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from './entities/cat.entity';
+import { BreedsService } from 'src/breeds/breeds.service';
+import { BreedsModule } from 'src/breeds/breeds.module';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Cat]), BreedsModule], // cargamos la entidad disponible en el contexto del módulos
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [CatsService, BreedsService],
+  exports: [],
 })
 export class CatsModule {}
