b4058b1f3c6677220f75f8d544015c05e9a36466
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-09 11:04:49 -0300

    funcionalidad de registro de usuarios con contraseñas protegidas

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,11 +5,11 @@ import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly AuthService: AuthService) {}
+    constructor(private readonly authService: AuthService) {}
 
     @Post("register")
     register(@Body() registerDto: RegisterDto) {
-        return 'registro de usuario exitoso';
+        return this.authService.register(registerDto)
     }
 
     @Post("login")
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as bcryptjs from "bcryptjs";
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UsersService) {}
+
+    async register({name, email, password}: CreateUserDto ) {
+        const user = this.userService.findOneByEmail(email);
+
+        if (!user) {
+            throw new BadRequestException('Email already exist');
+        }
+
+        // hash pass
+
+        const hashedPassword = await bcryptjs.hash(password, 10);
+
+
+        await this.userService.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        return {
+            message: "Usuario creado existosamente",
+        }
+    }
+
+    async login() {
+
+    }
+
+}
