5e2e8970bfc0868d3932a58c068e770163c64200
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-22 11:05:25 -0300

    acciones para las razas únicamente de los usuarios admin

--- src/breeds/breeds.controller.ts
+++ src/breeds/breeds.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { BreedsService } from './breeds.service';
 import { CreateBreedDto } from './dto/create-breed.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Role } from 'src/common/enums/role.enum';
 
+@Auth(Role.ADMIN)
 @Controller('breeds')
 export class BreedsController {
   constructor(private readonly breedsService: BreedsService) {}
