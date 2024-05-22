4ff5db97cda8ff2aecd699c9364a8b692c9f3c40
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-22 14:21:14 -0300

    endpoint users unicamente accedidos por usuarios admin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,8 +2,11 @@ import { Controller, Get, Post, Body, Patch, Param} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Role } from 'src/common/enums/role.enum';
 
 @Controller('users')
+@Auth(Role.ADMIN)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
